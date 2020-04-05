f39fb0206adbd5fa2539baa864c76e0d65e1645a
Achintya Kumar <ask60@duke.edu>
2020-04-05 18:44:12 -0400

    added bottom navigation from personal app, uses activity insteand of fragment

--- app/src/main/java/com/mantz_it/rfanalyzer/HomeActivity.java
+++ app/src/main/java/com/mantz_it/rfanalyzer/HomeActivity.java
@@ -1,18 +1,44 @@
 package com.mantz_it.rfanalyzer;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                        //overridePendingTransition(0,0);
+                        return true;
+                    case R.id.addComm:
+                        break;
+                    case R.id.settings:
+                        break;
+                    default:
+                        break;
+                }
+                return false;
+            }
+        });
     }
 
     @Override
