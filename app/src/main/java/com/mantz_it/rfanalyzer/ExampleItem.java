a47af548c8c6ce28bbf91b8a930c0e75713b1f18
Sahil Gupta <sg390@duke.edu>
2020-04-18 03:07:09 -0400

    updated gitignore

---
+++ app/src/main/java/com/mantz_it/rfanalyzer/ExampleItem.java
@@ -0,0 +1,13 @@
+package com.mantz_it.rfanalyzer;
+
+public class ExampleItem {
+    private String freq;
+
+    public ExampleItem(String freq) {
+        this.freq = freq;
+    }
+
+    public String getFreq() {
+        return this.freq;
+    }
+}
